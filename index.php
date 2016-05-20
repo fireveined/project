6ffa678a18cae51458e595ac4f2c9a99f12aa751
Mateusz Szymik <adde197@gmail.com>
2016-05-20 16:53:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
-// Kickstart the framework
+
 $f3=require('lib/base.php');
 
 $f3->set('DEBUG',1);
 if ((float)PCRE_VERSION<7.9)
 	trigger_error('PCRE version is out of date');
 
-// Load configuration
 $f3->config('config.ini');
 $f3->set('siteName','Faucet');
 
